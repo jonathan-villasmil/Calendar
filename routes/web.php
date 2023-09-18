33b92be0af078dc994fff6586b9670f5632450fa
jonathan <jonathanvillasmildeabreu@gmail.com>
2023-09-18 20:10:25 +0200

    laravel ui  adminlte:install --only=auth_views

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Auth::routes();
 Route::get('/home', function() {
     return view('home');
 })->name('home')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
